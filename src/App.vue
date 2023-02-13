46cc274b7b6eb1c49a2a104f7a3addeda3308956
Inoooooor <111541493+Inoooooor@users.noreply.github.com>
2023-02-14 02:00:15 +0300

    init commit

---
+++ src/App.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div class="container py-4 px-3 mx-auto row test">
+    <h1 class="">Hello, Bootstrap and Vite!</h1>
+    <button class="btn btn-primary">Primary button</button>
+  </div>
+</template>
+
+<style>
+</style>
---
+++ src/main.ts
@@ -0,0 +1,14 @@
+import { createApp } from 'vue'
+import { createPinia } from 'pinia'
+// import * as bootstrap from 'bootstrap'
+
+import App from './App.vue'
+
+import './assets/main.scss'
+import './assets/styles.scss'
+const app = createApp(App)
+
+app.use(createPinia())
+// app.use(bootstrap)
+
+app.mount('#app')

9706c73e6f474706b367c4bc480fcf45f46f7f5b
Inoooooor <111541493+Inoooooor@users.noreply.github.com>
2023-02-26 20:29:33 +0300

    asd

--- src/App.vue
+++ src/App.vue
@@ -2,10 +2,14 @@
 
 <template>
   <div class="container py-4 px-3 mx-auto row test">
-    <h1 class="">Hello, Bootstrap and Vite!</h1>
+    <h1 class="yellow">Hello, Bootstrap and Vite!</h1>
     <button class="btn btn-primary">Primary button</button>
+    <img src="./images/arrow.svg" alt="arrow" class="img-test" id="imgTest">
   </div>
 </template>
 
 <style>
+#imgTest {
+  width: 50%;
+}
 </style>
